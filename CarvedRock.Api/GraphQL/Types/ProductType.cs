4ad1783b3835517003ce16f7cfec2bcc2998ed84
Jassar alakhras <jalakhras@outlook.com>
2021-04-07 01:51:49 +0300

    using Data Loader to enhancement core performance

--- CarvedRock.Api/GraphQL/Types/ProductType.cs
+++ CarvedRock.Api/GraphQL/Types/ProductType.cs
@@ -1,12 +1,14 @@
 ﻿using CarvedRock.Api.Data.Entities;
 using CarvedRock.Api.Repositories;
+using GraphQL.DataLoader;
 using GraphQL.Types;
 
 namespace CarvedRock.Api.GraphQL.Types
 {
     public class ProductType: ObjectGraphType<Product>
     {
-        public ProductType(ProductReviewRepository productReviewRepository)
+        public ProductType(ProductReviewRepository reviewRepository,
+            IDataLoaderContextAccessor dataLoaderAccessor)
         {
             Field(t => t.Id);
             Field(t => t.Name).Description("The name of the product");
@@ -17,7 +19,15 @@ namespace CarvedRock.Api.GraphQL.Types
             Field(t => t.Rating).Description("The (max 5) star customer rating");
             Field(t => t.Stock);
             Field<ProductTypeEnumType>("Type", "The type of product");
-            Field<ListGraphType<ProductReviewType>>("review",resolve: context => productReviewRepository.GetForProduct(context.Source.Id));
+            Field<ListGraphType<ProductReviewType>>(
+                "reviews",
+                resolve: context =>
+                {
+                    var loader =
+                        dataLoaderAccessor.Context.GetOrAddCollectionBatchLoader<int, ProductReview>(
+                            "GetReviewsByProductId", reviewRepository.GetForProducts);
+                    return loader.LoadAsync(context.Source.Id);
+                });
         }
     }
 }
